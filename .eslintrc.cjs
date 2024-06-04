059cfc0e8a01788fdb87001f02ef01e0fabc77b1
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-06-04 10:46:15 -0700

    Set up eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+  },
+};
+module.exports = config;
